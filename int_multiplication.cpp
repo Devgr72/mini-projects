689c6c8fe7d59d8206abedea787ddf54f4fb4021
Dev Grover <Devgrover72@gmail.com>
2024-05-22 21:40:48 +0530

    Update int_multiplication.cpp

--- int_multiplication.cpp
+++ int_multiplication.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream.h>
+using namespace std;
+int main(){
+  int n1,n2;
+  cout<<"enter 1st number:"<<endl;
+  cin>>n1;
+  cout<<"enter 2nd number:"<<endl;
+  cin>>n2;
+  
+  return 0;
+}
